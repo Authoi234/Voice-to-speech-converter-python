2796861e893ec6ccede9110747df6751f258a4a4
Authoi234 <jawadkhan.authoi@gmail.com>
2025-07-17 17:26:00 +0600

    setup complete

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import speech_recognition as sr
+import pyperclip
+
+root = tk.Tk()
+root.title("Bangla speech to text converter By Authoi")
+T=tk.Text(root)
+T.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+tk.mainloop()
