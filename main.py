f22b6cc3a9b840d0463903ed303f9806d00e1cb5
Authoi234 <jawadkhan.authoi@gmail.com>
2025-07-20 21:06:43 +0600

    done

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import tkinter as tk
-import speech_recognition as sr
-import pyperclip
+import tkinter as tk, speech_recognition as sr, pyperclip
 
 def copy_text():
     text = T.get("1.0", tk.END)
     pyperclip.copy(text)
 
 def VoiceCommand():
-    recognizer = sr.Recognizer()
     voice_btn.config(text="Listening...", bg="yellow", fg="white")
 
     with sr.Microphone() as source:
         root.update()
 
         try:
-            audio = recognizer.listen(source, timeout=2)
-            recognizer.recognize_google(audio, language='bn-BD')
-            text = recognizer.recognize_google(audio, language='bn-BD')
+            audio =  sr.Recognizer().listen(source, timeout=2)
+            sr.Recognizer().recognize_google(audio, language='bn-BD')
+            text = sr.Recognizer().recognize_google(audio, language='bn-BD')
             T.insert(tk.END, text + " ")
             output_label.config(text="Completed")
         except sr.UnknownValueError:
@@ -24,7 +21,7 @@ def VoiceCommand():
         except sr.RequestError:
             output_label.config(text="Request Error")
         finally:
-            voice_btn.config(text="Voice", bg="lightgreen", fg="white")
+            voice_btn.config(text="Voice", bg="#AD8FE5", fg="white")
 
 root = tk.Tk()
 root.title("Bangla speech to text converter By Authoi")
@@ -37,7 +34,7 @@ btn_frame.pack(side=tk.TOP, fill=tk.X)
 copy_btn = tk.Button(btn_frame, text="Copy Text", bg="red", fg="white",font=("Helvetica", 10), width=20, height=2 , command=copy_text)
 copy_btn.pack(side=tk.TOP,   padx=15, pady=15)
 
-voice_btn = tk.Button(btn_frame, text="Voice", bg="lightgreen", fg="white",font=("Helvetica", 10), width=20, height=2 , command=VoiceCommand)
+voice_btn = tk.Button(btn_frame, text="Voice", bg="#AD8FE5", fg="white",font=("Helvetica", 10), width=20, height=2 , command=VoiceCommand)
 voice_btn.pack(side=tk.TOP,   padx=15, pady=15)
 
 output_label = tk.Label(root, text="Output", font=("Helvetica", 16))
